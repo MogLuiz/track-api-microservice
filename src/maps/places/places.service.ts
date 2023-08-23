c20d40368d82ee710ff5d50afa41ba6e88f249df
Luiz Henrique <luizhjramos@outlook.com>
2023-08-23 20:42:11 -0300

    feat: initialize directions resource

--- src/maps/places/places.service.ts
+++ src/maps/places/places.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { Client as GoogleMapsClient, PlaceInputType } from '@googlemaps/google-maps-services-js';
+import {
+  Client as GoogleMapsClient,
+  PlaceInputType,
+} from '@googlemaps/google-maps-services-js';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PlacesService {
-  constructor(private googleMapsClient: GoogleMapsClient, private configService: ConfigService) {}
+  constructor(
+    private googleMapsClient: GoogleMapsClient,
+    private configService: ConfigService,
+  ) { }
 
   async findPlace(text: string) {
     const { data } = await this.googleMapsClient.findPlaceFromText({
       params: {
         input: text,
         inputtype: PlaceInputType.textQuery,
-        fields: ['place_id','name', 'geometry', 'formatted_address'],
+        fields: ['place_id', 'name', 'geometry', 'formatted_address'],
         key: this.configService.get<string>('GOOGLE_MAPS_API_KEY'),
       },
     });
