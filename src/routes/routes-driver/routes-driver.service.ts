eaf5aca95dea0e370dec8b3545fcecd56c61bd2e
Luiz Henrique <luizhjramos@outlook.com>
2023-08-26 12:53:47 -0300

    feat: add RoutesDriver createrOrUpdate service

--- src/routes/routes-driver/routes-driver.service.ts
+++ src/routes/routes-driver/routes-driver.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
 
 @Injectable()
-export class RoutesDriverService {}
+export class RoutesDriverService {
+  constructor(private prismaService: PrismaService) { }
+
+  async createOrUpdate(dto: { route_id: string; lat: number; lgn: number }) {
+    return await this.prismaService.routeDriver.upsert({
+      include: {
+        route: true,
+      },
+      where: {
+        routeId: dto.route_id,
+      },
+      create: {
+        routeId: dto.route_id,
+        points: {
+          set: {
+            location: {
+              lat: dto.lat,
+              lng: dto.lgn,
+            },
+          },
+        },
+      },
+      update: {
+        points: {
+          push: {
+            location: {
+              lat: dto.lat,
+              lng: dto.lgn,
+            },
+          },
+        },
+      },
+    });
+  }
+}
