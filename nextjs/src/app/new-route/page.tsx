f926ec9df844247f00a4557b3ca9adb8dfc854c3
Luiz Henrique <luizhjramos@outlook.com>
2023-08-28 21:01:03 -0300

    feat: add data fetch error handle

--- nextjs/src/app/new-route/page.tsx
+++ nextjs/src/app/new-route/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { FormEvent } from "react";
+import type { FindPlaceFromTextResponseData } from "@googlemaps/google-maps-services-js";
 
 export default function NewRoutePage() {
   async function handleSearchPlaces(event: FormEvent) {
@@ -17,10 +18,31 @@ export default function NewRoutePage() {
       fetch(`http://localhost:3000/places?text=${destination}`),
     ]);
 
-    const [sourcePlaces, destinationPlaces] = await Promise.all([
-      sourceResponse.json(),
-      destinationResponse.json(),
-    ]);
+    const [sourcePlaces, destinationPlaces]: FindPlaceFromTextResponseData[] =
+      await Promise.all([sourceResponse.json(), destinationResponse.json()]);
+
+    if (sourcePlaces.status !== "OK") {
+      alert("Não foi possível encontrar o local de origem");
+      return;
+    }
+
+    if (destinationPlaces.status !== "OK") {
+      alert("Não foi possível encontrar o local de destino");
+      return;
+    }
+
+    const sourcePlaceId = sourcePlaces.candidates[0].place_id;
+    const destinationPlaceId = destinationPlaces.candidates[0].place_id;
+
+    const directionsResponse = await fetch(
+      `http://localhost:3000/directions?originId=${sourcePlaceId}&destinationId=${destinationPlaceId}`
+    );
+    const directionsData = await directionsResponse.json();
+
+    if (directionsData.status !== "OK") {
+      alert("Não foi possível encontrar uma rota");
+      return;
+    }
   }
 
   return (
